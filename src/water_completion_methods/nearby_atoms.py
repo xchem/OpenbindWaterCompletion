7c9553f8ab7a45fc38e9c4ad5e0e0e03a87e1ad5
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2025-12-02 14:39:23 +0000

    coot findwaters

--- src/water_completion_methods/nearby_atoms.py
+++ src/water_completion_methods/nearby_atoms.py
@@ -34,10 +34,11 @@ def get_ligand_waters(chain, res, st, threshold=7.0):
     print(st[0][chain])
     print(st[0][chain][res])
     for model in st:
-        for chain in model:
-            if chain.name == chain:
-                for res in chain:
-                    print(res)
+        for _chain in model:
+            if _chain.name == chain:
+                for _res in chain:
+                    print(_res)
+
     ligand_res = st[0][chain][res][0]
 
     # Get the ligand atoms
