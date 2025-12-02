352528f2c4b6edb02535b52f2235b8a40f6efafe
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2025-12-02 15:15:43 +0000

    coot findwaters

--- analysis/analyse_methods.py
+++ analysis/analyse_methods.py
@@ -61,6 +61,7 @@ def summarize_results(all_results):
             num_waters = ligand_results["num_waters"]
             print(f'\t\tRecall: {ligand_recall}')
             print(f'\t\tPrecision: {ligand_precision}')
+            print(f'\t\tNumber of water predictions: {num_waters}')
 
     ...
 
