a28a6318b57abc24150da1fc66f3a21b71541cbb
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2026-01-06 13:58:25 +0000

    findwaters multiple initial implementation

--- analysis/analyse_methods.py
+++ analysis/analyse_methods.py
@@ -17,7 +17,7 @@ def get_water_data(data_path, data, threshold=3.0):
             data_path / system / dtag / paths['ground'], 
                         data_path / system / dtag / paths['bound'], 
             data_path / system / dtag / paths['map'], 
-            get_ligand_waters(chain, res, st, threshold=3.0),
+            get_ligand_waters(chain, res, st, threshold=threshold),
             )
     
     return water_data
