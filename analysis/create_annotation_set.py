15f139799f3f647b1f3c8173a8ee350ac5c6972d
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2026-01-15 16:46:58 +0000

    make annotation data

--- analysis/create_annotation_set.py
+++ analysis/create_annotation_set.py
@@ -46,7 +46,7 @@ def get_mean_pos(res):
         pos = atom.pos
         poss.append([pos.x, pos.y, pos.z])
 
-    return np.mean(poss, axis=1)
+    return np.mean(poss, axis=0)
 
 def get_closest_ligand(bound_state_path, xyz):
     st = gemmi.read_structure(str(bound_state_path))
